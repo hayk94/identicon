b7b234479990454d223905f76693c956ac34e20b
Hayk <hayk_safaryan@epam.com>
2019-08-11 14:51:09 +0400

    implement draw_image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -19,6 +19,7 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
   end
 
   def hash_input(input) do
@@ -67,5 +68,15 @@ defmodule Identicon do
       {top_left, bottom_right}
     end
     %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+    :egd.render(image)
   end
 end
